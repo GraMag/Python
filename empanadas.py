c056ffa8a36a50e22e4e7a990da5df1839b05286
Mag <54412818+GraMag@users.noreply.github.com>
2020-02-17 00:41:02 -0300

    V3

--- empanadas.py
+++ empanadas.py
@@ -1,86 +1,110 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 empanadas = []
-titulos = [["User","J","C","H","A"]]
-jyq = 0
-carne = 0
-humita = 0
-atun = 0
-auxj = 0
-auxc = 0
-auxh = 0
-auxa = 0
-totalEmpanadas = 0
 
-
-price = float(input ("Precio por unidad: $"))
-print("")
-
-while True:
+# Ingresa el precio
+def unitPrice():
+	price = float(input ("Precio por unidad: $"))
+	print("")
+	return price
+	
+# Ingresa el nombre del usuario
+def user():
 	name = input("Tu nombre: ")
 	print("")
 	print("Hola", name)
-	# Input de gustos
-	auxj= int(input("Cuántas empanadas de jamon y queso querés? "))
-	jyq = jyq + auxj
-	auxc = int(input("Cuántas empanadas de carne querés? "))
-	carne = carne + auxc
-	auxh = int(input("Cuántas empanadas de humita querés? "))
-	humita = humita + auxh
-	auxa = int(input("Cuántas empanadas de atún querés? "))
-	atun = atun + auxa
+	return name
 	
-	# Añade el nombre de usuario y la cantidad de gustos seleccionados al final del array
-	empanadas.append([name, auxj, auxc, auxh, auxa])
+# Pide al usuario que ingrese un gusto
+def message(s):
+	taste = int(input("Cuántas empanadas de " + s + " querés? "))
+	return taste
 	
-	# Variable controladora para salir del While
+# Añadir usuarios
+def continuar():
 	print("")
 	cont = input("Desea ingresar otro usuario? S/N: ")
 	print("")
+	return cont
+
+# Añade a la lista cada perfil
+def order():
+	while True:
+		name = user()
+		# Input de gustos
+		auxj = message("jamon y queso")
+		auxc = message("carne") 
+		auxh = message("humita") 
+		auxa = message("atún") 
+		
+		# Añade el nombre de usuario y la cantidad de gustos seleccionados al final del array
+		empanadas.append([name, auxj, auxc, auxh, auxa])
+		
+		# Variable controladora para salir del While
+		if (continuar() == "N"):
+			break
+
+#Muestra el detalle del pedido.
+def total(price):
+	jyq = 0
+	carne = 0
+	humita = 0
+	atun = 0
 	
-	if (cont == "N"):
-		break	
-
-
-totalEmpanadas = jyq + carne + humita + atun
-importe = price*totalEmpanadas
-
-# Imprime el detalle del pedido.
-print("")
-print("El total de empanadas es:" , totalEmpanadas , ". De las cuales: ")
-print("Jamon y queso: " , jyq)
-print("Carne: " , carne)
-print("Humita: " , humita)
-print("Atún: " , atun)
-print("El total a pagar es: $" , (importe))
-print("")
-
-# Divide los gastos de modo proporcional o individual, muestra la lista, termina el programa.
-while True:	
+	for i in empanadas:
+		jyq = jyq + i[1]
+		carne = carne + i[2]
+		humita = humita + i[3]
+		atun = atun + i[4]
+	totalEmpanadas = int(jyq + carne + humita + atun )
+	amount = price * totalEmpanadas
 	
-	print("""Ingrese una opción:
-	1- Dividir la cuenta en partes iguales
-	2- Cada uno paga lo suyo
-	3- Mostrar lista
-	4- SALIR
-	""")
+	print("")
+	print("El total de empanadas es:" , totalEmpanadas , ". De las cuales: ")
+	print("Jamon y queso: " , jyq)
+	print("Carne: " , carne)
+	print("Humita: " , humita)
+	print("Atún: " , atun)
+	print("El total a pagar es: $" , (amount))
+	print("")
+	return amount
 
-	opcion = int(input("... "))
-	if opcion == 1:
-		print("Cada uno debe pagar: $" , (importe/len(empanadas)) )
-	if opcion == 2:
-		for i in empanadas:
-			suma = i[1] + i[2] + i[3] + i[4]
-			print(i[0], "pidio: ", suma, "empanadas. Le corresponde pagar: $", (price*suma) )
-			suma = 0
-	if opcion == 3:
-		for i in titulos:
-			print(i)
-		for gustos in empanadas:
-			print(gustos)
-	if opcion == 4:
-		print("Buen provecho.")
-		break
+# Divide los gastos de modo proporcional o individual, muestra la lista, termina el programa.	
+def menu(amount, empanadas):
+	titulos = [["Nombre","J","C","H","A"]]
+	while True:	
+	
+		print("""Ingrese una opción:
+		1- Dividir la cuenta en partes iguales
+		2- Cada uno paga lo suyo
+		3- Mostrar lista
+		4- SALIR
+		""")
 
+		option = int(input("... "))
+		if option == 1:
+			print("Cada uno debe pagar: $" , (amount/len(empanadas)) )
+		if option == 2:
+			for i in empanadas:
+				suma = i[1] + i[2] + i[3] + i[4]
+				print(i[0], "pidio: ", suma, "empanadas. Le corresponde pagar: $", (price*suma) )
+				suma = 0
+		if option == 3:
+			for i in titulos:
+				print(i)
+			for taste in empanadas:
+				print(taste)
+		if option == 4:
+			print("Buen provecho.")
+			break
+
+
+############
+##PROGRAMA##
+############
+
+
+price = unitPrice()
+order()
+menu(total(price), empanadas)
