2f748c899c1f86ffbaa32d128c959613ad6776e6
Mag <54412818+GraMag@users.noreply.github.com>
2020-03-02 16:21:42 -0300

    Happy Birthday song

--- happy birthday.py
+++ happy birthday.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy as np
+import winsound
+
+
+do = 262
+re = 294
+mi = 330
+fa = 349
+sol = 392
+la = 440
+si = 494
+
+winsound.Beep(294*2, 250)
+winsound.Beep(294*2, 250)
+winsound.Beep(330*2, 500)
+winsound.Beep(294*2, 500)
+winsound.Beep(494*2, 500)
+winsound.Beep(349*2, 750)
+
+winsound.Beep(294*2, 250)
+winsound.Beep(294*2, 250)
+winsound.Beep(330*2, 500)
+winsound.Beep(294*2, 500)
+winsound.Beep(440*2, 500)
+winsound.Beep(392*2, 750)
+
+winsound.Beep(294*2, 250)
+winsound.Beep(294*2, 250)
+winsound.Beep(294*3, 500)
+winsound.Beep(494*2, 500)
+winsound.Beep(392*2, 500)
+winsound.Beep(349*2, 500)
+winsound.Beep(330*2, 500)
+
+winsound.Beep(262*3, 250)
+winsound.Beep(262*3, 250)
+winsound.Beep(494*2, 500)
+winsound.Beep(392*2, 500)
+winsound.Beep(440*2, 500)
+winsound.Beep(392*2, 750)
